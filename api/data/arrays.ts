12b38bd23443d9026eb10e95c694243d45072e3b
unknown <sejfudin.duranovic@gmail.com>
2023-07-23 03:45:11 +0200

    database seed

---
+++ api/data/arrays.ts
@@ -0,0 +1,123 @@
+export const arrayWithPhoneNumbers = [
+    {
+      email: "marina@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0105",
+        },
+      ],
+    },
+    {
+      email: "kip@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0168",
+        },
+      ],
+    },
+    {
+      email: "lorie@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0162",
+        },
+      ],
+    },
+    {
+      email: "jasmin@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0168",
+        },
+      ],
+    },
+    {
+      email: "emma@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0187",
+        },
+      ],
+    },
+    {
+      email: "elvia@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0164",
+        },
+      ],
+    },
+    {
+      email: "liliana@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0161",
+        },
+      ],
+    },
+    {
+      email: "florencio@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0127",
+        },
+      ],
+    },
+    {
+      email: "delores@kahvana.com",
+      phoneNumbers: [
+        {
+          type: "primary",
+          value: "202-555-0143",
+        },
+      ],
+    },
+  ];
+  
+  export const arrayWithNames = [
+    {
+      email: "marina@kahvana.com",
+      name: "Marina",
+    },
+    {
+      email: "kip@kahvana.com",
+      name: "Kip",
+    },
+    {
+      email: "lorie@kahvana.com",
+      name: "Lorie",
+    },
+    {
+      email: "jasmin@kahvana.com",
+      name: "Jasmin",
+    },
+    {
+      email: "emma@kahvana.com",
+      name: "Emma",
+    },
+    {
+      email: "elvia@kahvana.com",
+      name: "Elvia",
+    },
+    {
+      email: "liliana@kahvana.com",
+      name: "Liliana",
+    },
+    {
+      email: "florencio@kahvana.com",
+      name: "Florencio",
+    },
+    {
+      email: "delores@kahvana.com",
+      name: "Dolores",
+    },
+  ];
+  
---
+++ api/mongo/seedData.ts
@@ -0,0 +1,26 @@
+import { combinedArray } from "../helper";
+import connectDB from "./db";
+import { connection } from "mongoose";
+import {UserModel} from '../models/user';
+  
+  const seedData = async () => {
+    try {
+      await connectDB();
+  
+       // Clear the existing collection (optional, if you want to start fresh)
+    await UserModel.deleteMany({});
+
+    // Insert the seed data
+    await UserModel.insertMany(combinedArray);
+
+    console.log('Data seeding completed');
+  
+      // Close the MongoDB connection
+      await connection.close();
+      console.log('MongoDB connection closed');
+    } catch (error) {
+      console.error('Error in seeding data:', error);
+    }
+  };
+  
+  seedData();
